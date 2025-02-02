ebc76cfee48a3fc7e0797c3f6bd5a33c81216b72
AbdAllah Khodeir <akhodeir3@gatech.edu>
2025-02-02 01:51:00 -0700

    Implemented cache for the get product service and logging

---
+++ src/main/java/com/example/nobsv2/CacheConfiguration.java
@@ -0,0 +1,36 @@
+package com.example.nobsv2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableCaching
+@EnableScheduling
+public class CacheConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(CacheConfiguration.class);
+
+    @Bean
+    public CacheManager cacheManager() {
+        ConcurrentMapCacheManager manager = new ConcurrentMapCacheManager();
+        manager.setAllowNullValues(false);
+        manager.setCacheNames(Arrays.asList("productCache"));
+        return manager;
+    }
+
+    @CacheEvict(value = "productCache", allEntries = true)
+    @Scheduled(fixedDelay = 7000, initialDelay = 0)
+    public void evictProductCache() {
+        logger.info("Evicting Product Cache");
+    }
+}
--- src/main/java/com/example/nobsv2/product/services/GetProductService.java
+++ src/main/java/com/example/nobsv2/product/services/GetProductService.java
@@ -5,6 +5,9 @@ import com.example.nobsv2.Query;
 import com.example.nobsv2.exceptions.ProductNotFoundException;
 import com.example.nobsv2.product.model.Product;
 import com.example.nobsv2.product.model.ProductDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +18,16 @@ public class GetProductService implements Query<Integer, ProductDTO> {
 
     private final ProductRepository productRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(GetProductService.class);
+
     public GetProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @Override
+    @Cacheable("productCache")
     public ResponseEntity<ProductDTO> execute(Integer id) {
+        logger.info("Executing " + getClass() + " ID: " + id);
         Optional<Product> productOptional = productRepository.findById(id);
         if (productOptional.isPresent()) {
             return ResponseEntity.ok(new ProductDTO(productOptional.get()));
